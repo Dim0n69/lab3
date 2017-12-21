1269d82a5805595cfcd3371634e72a3b646fd005
12345 <69pda69@gmail.com>
2017-12-22 02:31:27 +1000

    первая версия

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+string name;
+cout << "login: ";
+    while(true)
+    {
+        getline(cin, name);
+
+        if(!cin)
+        {
+            cout << "login incorrect\n";
+            cin.clear();
+             while(cin.get() !='\n');
+        }
+            else break;
+    }
+    cout << "Hello, " << name << endl;
+
+    return 0;
+}
