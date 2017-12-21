bb6c50af6d9933c00dc8647193abde8988b2ad77
12345 <69pda69@gmail.com>
2017-12-22 02:36:42 +1000

    3 версия

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
+#include <vector>
+#include <cstring>
 #include <iostream>
-
+#include <string>
+#include <iomanip>
+#include <locale.h>
+#include <algorithm>
 using namespace std;
+
+vector<string> new_users()
+{
+    vector<string> users;
+    users.push_back("user");
+    users.push_back("Dmitriy");
+ return users;
+}
 
 int main()
 {
     setlocale(LC_ALL, "rus");
 string name;
+
+vector<string> users = new_users();
 cout << "login: ";
     while(true)
     {
@@ -13,13 +28,19 @@ cout << "login: ";
 
         if(!cin)
         {
-            cout << "login incorrect\n";
+            cout << "login incorrect" << endl;
             cin.clear();
-             while(cin.get() !='\n');
+            while (cin.get() != '\n');
         }
             else break;
     }
-    cout << "Hello, " << name << endl;
+        if (find(users.begin(), users.end(), name) != users.end()) {
+        cout << "Hello, " << name << "!" << endl;
+    } else {
+        cout << "Hello, Guest!" << endl;
+    }
 
+
+    system("pause");
     return 0;
 }
